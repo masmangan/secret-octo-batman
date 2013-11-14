8b1c9e1f17f98f05dae01ddae30b96c3296dc9e4
mauriciocarvalho51 <mauricio_carvalho51@yahoo.com.br>
2013-11-14 11:24:07 -0200

    #10
    criado em 2 novas classes ...
    
    MyTableModel.class
    &
    JEmpresaPanel.class
    
    criar novo isseu para atualizar o EmpresaDAO como um novo metodo pra
    apresentar as empresas como um lista de itens ...

---
+++ src/margulis/gui/MyTableModel.java
@@ -0,0 +1,67 @@
+package margulis.gui;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import margulis.dao.EmpresaDAO;
+import margulis.pojo.Empresa;
+
+public class MyTableModel extends AbstractTableModel {
+
+	/**
+	 * @autor mauricio.victor.carvalho@gmail.com
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<Empresa> listadeempresas;
+
+	public MyTableModel() {
+		EmpresaDAO dao = new EmpresaDAO();
+		/*
+		listadeempresas = dao.findEmpresa();
+		
+		criar no EmpresaDAO um metodo 'findEmpresa()'
+		*/
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 3;
+	}
+
+	@Override
+	public int getRowCount() {
+		return listadeempresas.size();
+	}
+
+	@Override
+	public Object getValueAt(int lin, int col) {
+		Empresa l = listadeempresas.get(lin);
+		switch (col) {
+		case 0:
+			return l.getEmpId();
+		case 1:
+			return l.getNome();
+		case 2:
+			return l.getResponsavel();
+		default:
+			return "*ERRO*";
+		}		
+	}
+	
+	@Override
+	public String getColumnName(int col) {
+		switch (col) {
+		case 0:
+			return "cod";
+		case 1:
+			return "nome empresa";
+		case 2:
+			return "responsável";
+		default:
+			return "*ERRO*";
+		}
+	}
+}
+
