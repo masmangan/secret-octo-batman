82f58f05f9f4f309493ea466427fe996e24ac731
paulolimaesilva <paulo.limaesilva09@gmail.com>
2013-11-21 10:49:15 -0200

    Painel ValoresIniciais (correção 2) #21

--- src/margulis/gui/JValoresIniciais.java
+++ src/margulis/gui/JValoresIniciais.java
@@ -12,7 +12,17 @@ import javax.swing.Box;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+/**
+ * @author paulo.limaesilva09@gmail.com
+ */
+
 public class JValoresIniciais extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private JTextField textEstoqueInicial;
 	private JTextField textCustosVariaveis;
 	private JTextField textCustosFixos;
@@ -24,81 +34,82 @@ public class JValoresIniciais extends JPanel {
 	 * Create the panel.
 	 */
 	public JValoresIniciais() {
-		setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
-		
+		setLayout(new FormLayout(
+				new ColumnSpec[] { FormFactory.RELATED_GAP_COLSPEC,
+						FormFactory.DEFAULT_COLSPEC,
+						FormFactory.RELATED_GAP_COLSPEC,
+						FormFactory.DEFAULT_COLSPEC,
+						FormFactory.RELATED_GAP_COLSPEC,
+						ColumnSpec.decode("default:grow"),
+						FormFactory.RELATED_GAP_COLSPEC,
+						FormFactory.DEFAULT_COLSPEC, }, new RowSpec[] {
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC,
+						FormFactory.RELATED_GAP_ROWSPEC,
+						FormFactory.DEFAULT_ROWSPEC, }));
+
 		Component verticalStrut = Box.createVerticalStrut(20);
 		add(verticalStrut, "8, 2, 1, 17");
-		
+
 		JLabel lblValoresIniciais = new JLabel("Valores iniciais");
 		lblValoresIniciais.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		add(lblValoresIniciais, "4, 4");
-		
+
 		JLabel lblEstoqueInicial = new JLabel("Estoque inicial");
 		lblEstoqueInicial.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblEstoqueInicial, "4, 8, left, top");
-		
+
 		textEstoqueInicial = new JTextField();
 		add(textEstoqueInicial, "6, 8, fill, default");
 		textEstoqueInicial.setColumns(10);
-		
+
 		JLabel lblCustosVariaveis = new JLabel("Custos variaveis");
 		add(lblCustosVariaveis, "4, 10, left, default");
-		
+
 		textCustosVariaveis = new JTextField();
 		add(textCustosVariaveis, "6, 10, fill, default");
 		textCustosVariaveis.setColumns(10);
-		
+
 		JLabel lblCustosFixos = new JLabel("Custos fixos");
 		add(lblCustosFixos, "4, 12, left, default");
-		
+
 		textCustosFixos = new JTextField();
 		add(textCustosFixos, "6, 12, fill, default");
 		textCustosFixos.setColumns(10);
-		
-		JLabel lblDespesasAdministrativas = new JLabel("Despesas administrativas");
+
+		JLabel lblDespesasAdministrativas = new JLabel(
+				"Despesas administrativas");
 		add(lblDespesasAdministrativas, "4, 14, left, default");
-		
+
 		textDespesasAdministrativas = new JTextField();
 		add(textDespesasAdministrativas, "6, 14, fill, default");
 		textDespesasAdministrativas.setColumns(10);
-		
-		JLabel lblJurosSobreEmprstimos = new JLabel("Juros sobre empr\u00E9stimos banc\u00E1rios");
+
+		JLabel lblJurosSobreEmprstimos = new JLabel(
+				"Juros sobre empr\u00E9stimos banc\u00E1rios");
 		add(lblJurosSobreEmprstimos, "4, 16, left, default");
-		
+
 		textJurosSobreEmprestimo = new JTextField();
 		add(textJurosSobreEmprestimo, "6, 16, fill, default");
 		textJurosSobreEmprestimo.setColumns(10);
-		
+
 		JLabel lblValorDoImposto = new JLabel("Valor do imposto de renda");
 		add(lblValorDoImposto, "4, 18, left, default");
-		
+
 		textValorDoImposto = new JTextField();
 		textValorDoImposto.setText("");
 		add(textValorDoImposto, "6, 18, fill, default");
