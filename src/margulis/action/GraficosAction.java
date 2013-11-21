0d70db12052bc3e74c0199f5997b6a3a9f97da74
vaigu <vaigu@hotmail.com>
2013-11-21 10:47:32 -0200

    #34
    Criado Graficos Action, mas sem ação no momento.

---
+++ src/margulis/action/GraficosAction.java
@@ -0,0 +1,33 @@
+package margulis.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author gustavo.domingues@rocketmail.com
+ *
+ */
+
+public class GraficosAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	
+
+	public GraficosAction(JFrame frame) {
+		super("Graficos");
+		this.frame = frame;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Sem ação no momento");
+	}
+
+}
