6649e0e67e84a1d583307fc1d6d33224f5146e75
mauriciocarvalho51 <mauricio_carvalho51@yahoo.com.br>
2013-11-21 11:00:39 -0200

    Início do ConfiguraçãoPanel

---
+++ src/margulis/gui/JConfiguracaoPanel.java
@@ -0,0 +1,13 @@
+package margulis.gui;
+
+public class JConfiguracaoPanel extends JPanel {
+
+	/**
+	 * @author mauricio.victor.carvalho@gmail.com
+	 */
+	public JConfiguracaoPanel() {
+
+	}
+
+}
+	
