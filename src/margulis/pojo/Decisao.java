33c67a3a58e21a8847b8944176806404e84bf94b
thiagodiasads <thiagodiasads@gmail.com>
2013-11-25 17:03:18 -0200

    #9 Alterado tipos de dados de acordo com dados iniciais obtidos com cliente (issue #16)

--- src/margulis/pojo/Decisao.java
+++ src/margulis/pojo/Decisao.java
@@ -2,70 +2,59 @@ package margulis.pojo;
 
 /**
  * 
- * @author 
+ * @author Dias
  *
  */
 public class Decisao {
 	
 	private int decisaoid;
 	private int empid;
-	private String periodo;
+	private int rodada;
 	private double preco;
 	private double marketing;
-	private int quantidade;
-	
-	public Decisao(int decisaoid, int empid, String periodo, double preco,
-			double marketing, int quantidade) {
+	private int producao;
+	public Decisao(int empid, int rodada, double preco, double marketing,
+			int producao) {
+		super();
+		this.empid = empid;
+		this.rodada = rodada;
+		this.preco = preco;
+		this.marketing = marketing;
+		this.producao = producao;
+	}
+	public Decisao(int decisaoid, int empid, int rodada, double preco,
+			double marketing, int producao) {
 		super();
 		this.decisaoid = decisaoid;
 		this.empid = empid;
-		this.periodo = periodo;
+		this.rodada = rodada;
 		this.preco = preco;
 		this.marketing = marketing;
-		this.quantidade = quantidade;
+		this.producao = producao;
 	}
-	
-	public Decisao(int empid, String periodo, double preco, double marketing,
-			int quantidade) {
-		super();
-		this.empid = empid;
-		this.periodo = periodo;
-		this.preco = preco;
-		this.marketing = marketing;
-		this.quantidade = quantidade;
-	}
-
 	public int getDecisaoid() {
 		return decisaoid;
 	}
-
 	public int getEmpid() {
 		return empid;
 	}
-
-	public String getPeriodo() {
-		return periodo;
+	public int getRodada() {
+		return rodada;
 	}
-
 	public double getPreco() {
 		return preco;
 	}
-
 	public double getMarketing() {
 		return marketing;
 	}
-
-	public int getQuantidade() {
-		return quantidade;
+	public int getProducao() {
+		return producao;
 	}
-
 	@Override
 	public String toString() {
 		return String
-				.format("Decisao [decisaoid=%s, empid=%s, periodo=%s, preco=%s, marketing=%s, quantidade=%s]",
-						decisaoid, empid, periodo, preco, marketing, quantidade);
+				.format("Decisao [decisaoid=%s, empid=%s, rodada=%s, preco=%s, marketing=%s, producao=%s]",
+						decisaoid, empid, rodada, preco, marketing, producao);
 	}
-
-}
-
 	
+}
