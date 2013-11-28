76f6b0bfcd0bc4efe0aed2df6c159b6ce97eb0f8
andreirs <andreirs@outlook.com>
2013-11-28 09:28:34 -0200

    #42 - Criar teste unitário de um POJO
    Criado o teste unitário para o POJO Decisão, e adicionado os jars
    'junit-4.11.jar' e 'hamcrest-core-1.3.jar'.

---
+++ src/margulis/pojo/DecisaoTest.java
@@ -0,0 +1,132 @@
+package margulis.pojo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DecisaoTest {
+
+	@Test
+	public void testDecisaoIntIntStringDoubleDoubleInt() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 1;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(decisaoid, d.getDecisaoid());
+		assertEquals(empid, d.getEmpid());
+		assertEquals(rodada, d.getRodada());
+		assertEquals(preco, d.getPreco());
+		assertEquals(marketing, d.getMarketing());
+		assertEquals(producao, d.getProducao());
+	}
+
+	@Test
+	public void testDecisaoIntStringDoubleDoubleInt() {
+		int empid = 6;
+		int rodada = 3;
+		double preco = 153.50;
+		double marketing = 560.20;
+		int producao = 80;
+		Decisao d = new Decisao(empid, rodada, preco, marketing, producao);
+		assertEquals(empid, d.getEmpid());
+		assertEquals(rodada, d.getRodada());
+		assertEquals(preco, d.getPreco());
+		assertEquals(marketing, d.getMarketing());
+		assertEquals(producao, d.getProducao());
+	}
+
+	@Test
+	public void testGetDecisaoid() {
+		int decisaoid = 6;
+		int empid = 6;
+		int rodada = 3;
+		double preco = 153.50;
+		double marketing = 560.20;
+		int producao = 80;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(decisaoid, d.getDecisaoid());
+	}
+
+	@Test
+	public void testGetEmpid() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 22;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(empid, d.getEmpid());
+	}
+
+	@Test
+	public void testGetRodada() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 5;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(rodada, d.getRodada());
+	}
+
+	@Test
+	public void testGetPreco() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 2;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(preco, d.getPreco());
+	}
+
+	@Test
+	public void testGetMarketing() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 2;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(marketing, d.getMarketing());
+	}
+
+	@Test
+	public void testGetQuantidade() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 2;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		assertEquals(producao, d.getProducao());
+	}
+
+	@Test
+	public void testToString() {
+		int decisaoid = 5;
+		int empid = 4;
+		int rodada = 1;
+		double preco = 124.50;
+		double marketing = 550.80;
+		int producao = 153;
+		Decisao d = new Decisao(decisaoid, empid, rodada, preco, marketing, producao);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("Decisao [decisaoid="+decisaoid+", "
+				+ "empid="+empid+", rodada="+rodada+", "
+				+ "preco="+preco+", marketing="+marketing+", producao="+producao+"]");
+		
+		assertEquals(sb.toString(), d.toString());
+	}
+
+}
