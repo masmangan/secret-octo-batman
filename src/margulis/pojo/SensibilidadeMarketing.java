c97f0aad3fcd022e4869e5221ca91ca7824e7db2
Marco Mangan <marco.mangan@gmail.com>
2013-12-21 12:36:24 -0200

    #60

---
+++ src/margulis/pojo/SensibilidadeMarketing.java
@@ -0,0 +1,32 @@
+package margulis.pojo;
+
+public class SensibilidadeMarketing {
+
+	public static double intervalo = 0.0033;
+
+	public static double[][] tabela = { { 0, 0.0000 }, { 1000, 0.0033 },
+			{ 4500, 0.0067 }, { 8000, 0.0100 }, { 11500, 0.0133 },
+			{ 15000, 0.0167 }, { 18500, 0.0200 }, { 22000, 0.0233 },
+			{ 25500, 0.0267 }, { 29000, 0.0300 }, { 32500, 0.0333 },
+			{ 36000, 0.0367 }, { 39500, 0.0400 }, { 43000, 0.0433 },
+			{ 46500, 0.0467 }, { 50000, 0.0500 }, { 53500, 0.0533 },
+			{ 57000, 0.0567 }, { 60500, 0.0600 }, { 64000, 0.0633 },
+			{ 67500, 0.0667 }, { 71000, 0.0700 }, { 74500, 0.0733 },
+			{ 78000, 0.0767 }, { 81500, 0.0800 }, { 85000, 0.0833 },
+			{ 88500, 0.0867 }, { 92000, 0.0900 }, { 95500, 0.0933 },
+			{ 99000, 0.0967 }, { 100000, 0.1000 } };
+
+	public static double getSensibilidade(double valor) {
+		if (valor < tabela[0][0]) {
+			throw new IllegalArgumentException();
+		}
+		double sensibilidade = 0;
+		int i = 0;
+		while (valor >= tabela[i][0]) {
+			i++;
+		}
+		i--;
+		sensibilidade = tabela[i][1];
+		return sensibilidade;
+	}
+}
---
+++ src/margulis/pojo/SensibilidadeMarketingTest.java
@@ -0,0 +1,50 @@
+package margulis.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SensibilidadeMarketingTest {
+
+	@Test(expected = IllegalArgumentException.class)  
+	public final void testTooSmall() {
+		SensibilidadeMarketing.getSensibilidade(-1);
+		fail();
+	}
+	@Test  
+	public final void testZero() {
+		double actual = SensibilidadeMarketing.getSensibilidade(0.0);
+		double expected = 0.0;
+		assertEquals(expected, actual, 0.0001);
+	}
+	@Test  
+	public final void testOne() {
+		double actual = SensibilidadeMarketing.getSensibilidade(1.0);
+		double expected = 0.0;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierAbove() {
+		double actual = SensibilidadeMarketing.getSensibilidade(1000.0);
+		double expected = 0.0033;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierBellow() {
+		double actual = SensibilidadeMarketing.getSensibilidade(999.0);
+		double expected = 0.0;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierBellowBellow() {
+		double actual = SensibilidadeMarketing.getSensibilidade(998.0);
+		double expected = 0.0;
+		assertEquals(expected, actual, 0.0001);
+	}
+	@Test  
+	public final void testLastTier() {
+		double actual = SensibilidadeMarketing.getSensibilidade(57000.0);
+		double expected = 0.0567;
+		assertEquals(expected, actual, 0.0001);
+	}
+}
