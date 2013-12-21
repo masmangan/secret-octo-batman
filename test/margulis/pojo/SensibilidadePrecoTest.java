67c49cde031866175ee0ad4379e6aaacb8ad9b91
Marco Mangan <marco.mangan@gmail.com>
2013-12-21 13:00:02 -0200

    #61

---
+++ test/margulis/pojo/SensibilidadePrecoTest.java
@@ -0,0 +1,50 @@
+package margulis.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SensibilidadePrecoTest {
+
+	@Test(expected = IllegalArgumentException.class)  
+	public final void testTooSmall() {
+		SensibilidadePreco.getSensibilidade(-1);
+		fail();
+	}
+	@Test  
+	public final void testZero() {
+		double actual = SensibilidadePreco.getSensibilidade(0.0);
+		double expected = 0.9;
+		assertEquals(expected, actual, 0.0001);
+	}
+	@Test  
+	public final void testOne() {
+		double actual = SensibilidadePreco.getSensibilidade(1.0);
+		double expected = 0.9;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierAbove() {
+		double actual = SensibilidadePreco.getSensibilidade(5.0);
+		double expected = 0.85;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierBellow() {
+		double actual = SensibilidadePreco.getSensibilidade(4.0);
+		double expected = 0.9;
+		assertEquals(expected, actual, 0.0001);
+	}	
+	@Test  
+	public final void testFirstTierBellowBellow() {
+		double actual = SensibilidadePreco.getSensibilidade(3.0);
+		double expected = 0.9;
+		assertEquals(expected, actual, 0.0001);
+	}
+	@Test  
+	public final void testLastTier() {
+		double actual = SensibilidadePreco.getSensibilidade(275.0);
+		double expected = -1.0;
+		assertEquals(expected, actual, 0.0001);
+	}
+}
